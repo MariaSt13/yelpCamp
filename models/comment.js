b6057b332af2186bb65f819e0698d63a448d51fd
MariaSt13 <mariast13@gmail.com>
2020-02-19 19:03:19 +0200

    added time since created for campgrounds and comments

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 // Define comment schema
 const commentSchema = new mongoose.Schema({
 	text: String,
+	createdAt: {type:Date, default: Date.now},
 	author: {
 		id: {
 			type: mongoose.Schema.Types.ObjectId,
